90d16fffdd2e57569229f231d50190d90e7cf4f9
bogdan-nazarenko <bogdannazzarenko@gmail.com>
2025-08-14 00:41:54 +0300

    feat: section and image removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
-import reactLogo from "./assets/media/images/react.svg";
 import "./styles/App.scss";
 
 function App() {
     return (
         <>
             <main>
-                <section>
-                    <div className="container">
-                        <div className="hero">
-                            <img src={reactLogo} alt="React" />
-                            <h1>React + Vite + SCSS + TS</h1>
-                        </div>
-                    </div>
-                </section>
             </main>
         </>
     );
