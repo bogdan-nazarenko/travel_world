982bfd0db1ff6e575c41e53d9b8e689dd521abba
bogdan-nazarenko <bogdannazzarenko@gmail.com>
2025-08-17 23:00:56 +0300

    feat: main (part of code was moved)

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
 import "./styles/helpers/reset.scss";
 import "./styles/helpers/vars.scss";
 import "./styles/main.scss";
-import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
