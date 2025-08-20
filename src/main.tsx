0875109b77b15de2e43d92108ec128c9726b9da6
bogdan-nazarenko <bogdannazzarenko@gmail.com>
2025-08-20 14:57:20 +0300

    feat: fonts

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./styles/helpers/reset.scss";
 import "./styles/helpers/vars.scss";
+import "./styles/fonts.scss";
 import "./styles/main.scss";
 
 createRoot(document.getElementById("root")!).render(
