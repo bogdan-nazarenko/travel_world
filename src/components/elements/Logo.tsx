2a325190264c7ea4608c83573856ccf532be6e28
bogdan-nazarenko <bogdannazzarenko@gmail.com>
2025-08-21 21:11:57 +0300

    feat: added property for logo (function)

--- src/components/elements/Logo.tsx
+++ src/components/elements/Logo.tsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
+import type { ClickProps } from "../helpers/interfaces";
 import logo from "../../assets/media/images/logo.png";
 import "../../styles/elements/Logo.scss";
 
-export const Logo = () => {
+export const Logo = ({ clickFunc }: ClickProps) => {
     return (
-        <Link className="logo" to="/">
+        <Link className="logo" to="/" onClick={clickFunc}>
             <img className="logo__pic" src={logo} alt="Travel World" />
         </Link>
     );
