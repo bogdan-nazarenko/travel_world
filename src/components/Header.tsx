db29252f8a03f66d4834c4cd88fdae938a3ceed8
bogdan-nazarenko <bogdannazzarenko@gmail.com>
2025-08-22 20:53:10 +0300

    feat: menu button finished

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -133,7 +133,25 @@ const Header = () => {
                 <div
                     className={`header__menu__button ${isMenuOpen}`}
                     onClick={toggleMenu}
-                ></div>
+                >
+                    <svg className="menu__icon" viewBox="0 0 24 24" fill="none">
+                        <path
+                            d="M4 6L20 6"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                        />
+                        <path
+                            d="M4 12L20 12"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                        />
+                        <path
+                            d="M4 18L20 18"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                        />
+                    </svg>
+                </div>
             </div>
         </header>
     );
