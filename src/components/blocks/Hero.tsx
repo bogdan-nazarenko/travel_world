135a4a68d6516fbb603ed4d78f084f133fe50858
bogdan-nazarenko <bogdannazzarenko@gmail.com>
2025-09-02 23:16:01 +0300

    feat: imported SearchBar, changed hero style

--- src/components/blocks/Hero.tsx
+++ src/components/blocks/Hero.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { SearchBar } from "../elements/SearchBar.tsx";
 import worldIcon from "../../assets/media/images/icons/world.png";
 import heroImage1 from "../../assets/media/images/hero_img_1.jpg";
 import heroImage2 from "../../assets/media/images/hero_img_2.jpg";
@@ -60,68 +61,73 @@ export const Hero = () => {
     return (
         <section className="hero_section">
             <div className="container hero">
-                <div className="hero__info">
-                    <span className="block__suptitle suptitle--v1 hero__suptitle">
-                        Know Before You Go{" "}
-                        <img
-                            className="world__icon"
-                            src={worldIcon}
-                            alt="World"
-                        />
-                    </span>
-                    <h1 className="hero__title">
-                        Traveling opens the door to creating{" "}
-                        <span className="hero__title--highlight">memories</span>
-                    </h1>
-                    <p className="block__text text--v1">
-                        Lorem ipsum dolor sit amet consectetur, adipisicing
-                        elit. Ullam ipsum nobis asperiores soluta voluptas quas
-                        voluptates. Molestiae tempora dignissimos, animi
-                        praesentium molestias perferendis porro expedita
-                        delectus. Soluta natus porro.
-                    </p>
+                <div className="hero__content">
+                    <div className="hero__info">
+                        <span className="block__suptitle suptitle--v1 hero__suptitle">
+                            Know Before You Go{" "}
+                            <img
+                                className="world__icon"
+                                src={worldIcon}
+                                alt="World"
+                            />
+                        </span>
+                        <h1 className="hero__title">
+                            Traveling opens the door to creating{" "}
+                            <span className="hero__title--highlight">
+                                memories
+                            </span>
+                        </h1>
+                        <p className="block__text text--v1">
+                            Lorem ipsum dolor sit amet consectetur, adipisicing
+                            elit. Ullam ipsum nobis asperiores soluta voluptas
+                            quas voluptates. Molestiae tempora dignissimos,
+                            animi praesentium molestias perferendis porro
+                            expedita delectus. Soluta natus porro.
+                        </p>
+                    </div>
+                    <div className="hero__media">
+                        <div className="hero__media__content">
+                            <img
+                                className="hero__pic"
+                                src={heroImage1}
+                                alt="Hero image"
+                            />
+                        </div>
+                        <div className="hero__media__content">
+                            <video
+                                className={"hero__video"}
+                                src={heroVideo}
+                                style={{
+                                    objectFit: `${view ? "contain" : "cover"}`,
+                                }}
+                                onClick={videoPlay}
+                                onDoubleClick={videoFullScreen}
+                            ></video>
+                            <button
+                                className="video__control"
+                                style={{
+                                    backgroundImage: `url("${isPlay ? play : pause}")`,
+                                }}
+                                onClick={videoPlay}
+                            ></button>
+                            <button
+                                className="mobile__full__screen"
+                                style={{
+                                    backgroundImage: `url("${fullScreen}")`,
+                                }}
+                                onClick={videoFullScreen}
+                            ></button>
+                        </div>
+                        <div className="hero__media__content">
+                            <img
+                                className="hero__pic"
+                                src={heroImage2}
+                                alt="Hero image"
+                            />
+                        </div>
+                    </div>
                 </div>
-                <div className="hero__media">
-                    <div className="hero__media__content">
-                        <img
-                            className="hero__pic"
-                            src={heroImage1}
-                            alt="Hero image"
-                        />
-                    </div>
-                    <div className="hero__media__content">
-                        <video
-                            className={"hero__video"}
-                            src={heroVideo}
-                            style={{
-                                objectFit: `${view ? "contain" : "cover"}`,
-                            }}
-                            onClick={videoPlay}
-                            onDoubleClick={videoFullScreen}
-                        ></video>
-                        <button
-                            className="video__control"
-                            style={{
-                                backgroundImage: `url("${isPlay ? play : pause}")`,
-                            }}
-                            onClick={videoPlay}
-                        ></button>
-                        <button
-                            className="mobile__full__screen"
-                            style={{
-                                backgroundImage: `url("${fullScreen}")`,
-                            }}
-                            onClick={videoFullScreen}
-                        ></button>
-                    </div>
-                    <div className="hero__media__content">
-                        <img
-                            className="hero__pic"
-                            src={heroImage2}
-                            alt="Hero image"
-                        />
-                    </div>
-                </div>
+                <SearchBar />
             </div>
         </section>
     );
