56dec1ee026bf9df8b631c5b42a2d7830fedefd3
bogdan-nazarenko <bogdannazzarenko@gmail.com>
2025-09-07 14:15:57 +0300

    feat: services block added

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,9 +1,11 @@
 import { Hero } from "../blocks/Hero.tsx";
+import { Services } from "../blocks/Services.tsx";
 
 const Home = () => {
     return (
         <>
             <Hero />
+            <Services suptitleColor={"--light-pink-color"} />
         </>
     );
 };
