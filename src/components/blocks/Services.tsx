a45007d9709c58681bad6ef1571ec33d1be4ee9d
bogdan-nazarenko <bogdannazzarenko@gmail.com>
2025-09-07 18:05:31 +0300

    feat: services block

---
+++ src/components/blocks/Services.tsx
@@ -0,0 +1,49 @@
+import type { ServicesProps } from "../helpers/types.tsx";
+import "../../styles/blocks/Services.scss";
+
+export const Services = ({ suptitleColor }: ServicesProps) => {
+    return (
+        <section className="services_section">
+            <div className="container services">
+                <div className="services__info">
+                    <div
+                        className="services__suptitle"
+                        style={{ color: `var(${suptitleColor})` }}
+                    >
+                        What we serve
+                    </div>
+                    <h2 className="block__title title--v3">
+                        We offer our best services
+                    </h2>
+                </div>
+                <div className="services__elements">
+                    <div className="services__item">
+                        <div className="services__item__image"></div>
+                        <h3 className="services__item__title">
+                            Calculate Weather
+                        </h3>
+                        <p className="block__text text--v3">
+                            Lorem ipsum dolor sit amet, adipisicing elit.
+                        </p>
+                    </div>
+                    <div className="services__item">
+                        <div className="services__item__image"></div>
+                        <h3 className="services__item__title">
+                            Best Tour Guide
+                        </h3>
+                        <p className="block__text text--v3">
+                            Lorem ipsum dolor sit amet, adipisicing elit.
+                        </p>
+                    </div>
+                    <div className="services__item">
+                        <div className="services__item__image"></div>
+                        <h3 className="services__item__title">Customization</h3>
+                        <p className="block__text text--v3">
+                            Lorem ipsum dolor sit amet, adipisicing elit.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
