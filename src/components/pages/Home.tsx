85cbcefda30bb7ab9378c8d36839ce9d8df1c661
bogdan-nazarenko <bogdannazzarenko@gmail.com>
2025-09-08 18:50:37 +0300

    feat: explore block added

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,11 +1,13 @@
 import { Hero } from "../blocks/Hero.tsx";
 import { Services } from "../blocks/Services.tsx";
+import { Explore } from "../blocks/Explore.tsx";
 
 const Home = () => {
     return (
         <>
             <Hero />
             <Services suptitleColor={"--light-pink-color"} />
+            <Explore />
         </>
     );
 };
