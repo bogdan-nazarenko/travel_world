215bb648820800ba8a28c4304f69b11f990b61dc
bogdan-nazarenko <bogdannazzarenko@gmail.com>
2025-09-11 20:39:05 +0300

    feat: start of gallery block

---
+++ src/components/blocks/Gallery.tsx
@@ -0,0 +1,7 @@
+// import { Fancybox } from "@fancyapps/ui/dist/fancybox/";
+import "@fancyapps/ui/dist/fancybox/fancybox.css";
+import "../../styles/blocks/Gallery.scss";
+
+export const Gallery = () => {
+    return <></>;
+};
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -2,6 +2,7 @@ import { Hero } from "../blocks/Hero.tsx";
 import { Services } from "../blocks/Services.tsx";
 import { Explore } from "../blocks/Explore.tsx";
 import { Experience } from "../blocks/Experience.tsx";
+import { Gallery } from "../blocks/Gallery.tsx";
 
 const Home = () => {
     return (
@@ -10,6 +11,7 @@ const Home = () => {
             <Services suptitleColor={"--light-pink-color"} />
             <Explore />
             <Experience />
+            <Gallery />
         </>
     );
 };
