900f3ee37c6311b1aa5fab4e55db8af98149aef0
bogdan-nazarenko <bogdannazzarenko@gmail.com>
2025-09-13 15:35:16 +0300

    feat: gallery (layout)

--- src/components/blocks/Gallery.tsx
+++ src/components/blocks/Gallery.tsx
@@ -1,7 +1,98 @@
 // import { Fancybox } from "@fancyapps/ui/dist/fancybox/";
 import "@fancyapps/ui/dist/fancybox/fancybox.css";
+import galleryImg1 from "../../assets/media/images/gallery_img_1.jpg";
+import galleryImg2 from "../../assets/media/images/gallery_img_2.jpg";
+import galleryImg3 from "../../assets/media/images/gallery_img_3.jpg";
+import galleryImg4 from "../../assets/media/images/gallery_img_4.jpg";
+import galleryImg5 from "../../assets/media/images/gallery_img_5.jpg";
+import galleryImg6 from "../../assets/media/images/gallery_img_6.jpg";
+import galleryImg7 from "../../assets/media/images/gallery_img_7.jpg";
+import galleryImg8 from "../../assets/media/images/gallery_img_8.jpg";
 import "../../styles/blocks/Gallery.scss";
 
 export const Gallery = () => {
-    return <></>;
+    return (
+        <section id="gallery_section">
+            <div className="container gallery">
+                <div className="gallery__info">
+                    <span className="block__suptitle suptitle--v2">
+                        Gallery
+                    </span>
+                    <h2 className="block__title title--v1">
+                        Visit our customers tour gallery
+                    </h2>
+                </div>
+                <div className="gallery__images">
+                    <div className="gallery__column__group">
+                        <div className="gallery__column">
+                            <div className="gallery__image">
+                                <img
+                                    className="gallery__pic"
+                                    src={galleryImg1}
+                                    alt="Gallery picture 1"
+                                />
+                            </div>
+                            <div className="gallery__image">
+                                <img
+                                    className="gallery__pic"
+                                    src={galleryImg2}
+                                    alt="Gallery picture 2"
+                                />
+                            </div>
+                        </div>
+                        <div className="gallery__column">
+                            <div className="gallery__image">
+                                <img
+                                    className="gallery__pic"
+                                    src={galleryImg3}
+                                    alt="Gallery picture 3"
+                                />
+                            </div>
+                            <div className="gallery__image">
+                                <img
+                                    className="gallery__pic"
+                                    src={galleryImg4}
+                                    alt="Gallery picture 4"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="gallery__column__group">
+                        <div className="gallery__column">
+                            <div className="gallery__image">
+                                <img
+                                    className="gallery__pic"
+                                    src={galleryImg5}
+                                    alt="Gallery picture 5"
+                                />
+                            </div>
+                            <div className="gallery__image">
+                                <img
+                                    className="gallery__pic"
+                                    src={galleryImg6}
+                                    alt="Gallery picture 6"
+                                />
+                            </div>
+                        </div>
+                        <div className="gallery__column">
+                            <div className="gallery__image">
+                                <img
+                                    className="gallery__pic"
+                                    src={galleryImg7}
+                                    alt="Gallery picture 7"
+                                />
+                            </div>
+                            <div className="gallery__image">
+                                <img
+                                    className="gallery__pic"
+                                    src={galleryImg8}
+                                    alt="Gallery picture 8"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 };
