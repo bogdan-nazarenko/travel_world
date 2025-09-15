210e84d763a09c541c103267728a3c670b078a6e
bogdan-nazarenko <bogdannazzarenko@gmail.com>
2025-09-15 11:25:54 +0300

    fix: added HashRouter (for SPA on GitHub Pages), fixed error page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header.tsx";
 import Home from "./components/pages/Home.tsx";
 import About from "./components/pages/About.tsx";
@@ -10,7 +10,7 @@ import Footer from "./components/Footer.tsx";
 
 const App = () => {
     return (
-        <BrowserRouter basename="/deploy_travel_world/">
+        <HashRouter>
             <Header />
             <main>
                 <Routes>
@@ -19,12 +19,11 @@ const App = () => {
                     <Route path="/tours" element={<Tours />}></Route>
                     <Route path="/login" element={<Login />}></Route>
                     <Route path="/register" element={<Register />}></Route>
+                    <Route path="*" element={<ErrorPage />}></Route>
                 </Routes>
-
-                <ErrorPage />
             </main>
             <Footer />
-        </BrowserRouter>
+        </HashRouter>
     );
 };
 
--- src/components/pages/ErrorPage.tsx
+++ src/components/pages/ErrorPage.tsx
@@ -1,22 +1,5 @@
-import { useLocation } from "react-router-dom";
-import type { CustomLocation } from "../helpers/types.tsx";
-
-const Error = () => {
-    return <h2 style={{ color: "red" }}>Error 404</h2>;
-};
-
 const ErrorPage = () => {
-    const location: CustomLocation = useLocation();
-
-    return (
-        <>
-            {location.pathname !== "/" &&
-                location.pathname !== "/about" &&
-                location.pathname !== "/tours" &&
-                location.pathname !== "/login" &&
-                location.pathname !== "/register" && <Error />}
-        </>
-    );
+    return <h2 style={{ color: "red" }}>Error 404</h2>;
 };
 
 export default ErrorPage;
