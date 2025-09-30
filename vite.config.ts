ccdd8e41394a8a3480ae196831d654e31a29ab86
bogdan-nazarenko <bogdannazzarenko@gmail.com>
2025-10-01 00:41:04 +0300

    feat: added option for build

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,17 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     plugins: [react()],
     base: "/travel_world/",
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes("node_modules")) {
+                        return "vendor";
+                    }
+
+                    return null;
+                },
+            },
+        },
+    },
 });
