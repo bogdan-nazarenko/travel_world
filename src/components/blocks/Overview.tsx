e6806012b856788f08c5d8bd58b996b4b9952c82
bogdan-nazarenko <bogdannazzarenko@gmail.com>
2025-10-02 04:13:15 +0300

    feat: overview block

---
+++ src/components/blocks/Overview.tsx
@@ -0,0 +1,43 @@
+import overviewImage from "../../assets/media/images/gallery_img_2.jpg";
+import "../../styles/blocks/Overview.scss";
+
+const Overview = () => {
+    return (
+        <section className="overview_section">
+            <div className="container overview">
+                <div className="overview__info">
+                    <div className="overview__info__item">
+                        <h2 className="block__title title--v4 overview__title">
+                            Who <span>we are?</span>
+                        </h2>
+                        <p className="overview__text">
+                            We’re all about creating unforgettable experiences
+                            for our guests. Our journey began with a simple
+                            passion for exploring the beauty of the World.
+                        </p>
+                    </div>
+                    <div className="overview__info__item">
+                        <h2 className="block__title title--v4 overview__title">
+                            Our <span>mission</span>
+                        </h2>
+                        <p className="overview__text">
+                            We believe that travel is not just about visiting
+                            new places, but about immersing yourself in new
+                            cultures, connecting with nature, and making
+                            memories that last a lifetime.
+                        </p>
+                    </div>
+                </div>
+                <div className="overview__image">
+                    <img
+                        className="overview__pic"
+                        src={overviewImage}
+                        alt="Overview picture"
+                    />
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Overview;
--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -1,11 +1,12 @@
 import Panorama from "../blocks/Panorama.tsx";
-
+import Overview from "../blocks/Overview.tsx";
 import Services from "../blocks/Services.tsx";
 
 const About = () => {
     return (
         <>
             <Panorama />
+            <Overview />
             <Services suptitleColor={"--primary-color"} />
         </>
     );
