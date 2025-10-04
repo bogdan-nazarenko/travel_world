49c2b814a4c4eddd568cad1e262c3998057e8cdf
bogdan-nazarenko <bogdannazzarenko@gmail.com>
2025-10-04 20:09:18 +0300

    feat: dynamic import used

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
+import { Suspense, lazy } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import Header from "./components/Header.tsx";
-import Home from "./components/pages/Home.tsx";
-import About from "./components/pages/About.tsx";
-import Tours from "./components/pages/Tours.tsx";
-import Login from "./components/pages/Login.tsx";
-import Register from "./components/pages/Register.tsx";
-import ErrorPage from "./components/pages/ErrorPage.tsx";
-import Newsletter from "./components/blocks/Newsletter.tsx";
+const Home = lazy(() => import("./components/pages/Home.tsx"));
+const About = lazy(() => import("./components/pages/About.tsx"));
+const Tours = lazy(() => import("./components/pages/Tours.tsx"));
+const Login = lazy(() => import("./components/pages/Login.tsx"));
+const Register = lazy(() => import("./components/pages/Register.tsx"));
+const ErrorPage = lazy(() => import("./components/pages/ErrorPage.tsx"));
+const Newsletter = lazy(() => import("./components/blocks/Newsletter.tsx"));
 import Footer from "./components/Footer.tsx";
 
 const App = () => {
@@ -20,16 +21,18 @@ const App = () => {
         <>
             <Header />
             <main>
-                <Routes>
-                    <Route path="/" element={<Home />}></Route>
-                    <Route path="/about" element={<About />}></Route>
-                    <Route path="/tours" element={<Tours />}></Route>
-                    <Route path="/login" element={<Login />}></Route>
-                    <Route path="/register" element={<Register />}></Route>
-                    <Route path="*" element={<ErrorPage />}></Route>
-                </Routes>
+                <Suspense fallback="Loading...">
+                    <Routes>
+                        <Route path="/" element={<Home />}></Route>
+                        <Route path="/about" element={<About />}></Route>
+                        <Route path="/tours" element={<Tours />}></Route>
+                        <Route path="/login" element={<Login />}></Route>
+                        <Route path="/register" element={<Register />}></Route>
+                        <Route path="*" element={<ErrorPage />}></Route>
+                    </Routes>
 
-                {isMainPath && <Newsletter />}
+                    {isMainPath && <Newsletter />}
+                </Suspense>
             </main>
             <Footer />
         </>
