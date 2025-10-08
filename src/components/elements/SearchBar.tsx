716c101b0cd749be2ff0728ef584ecb0bb38153c
bogdan-nazarenko <bogdannazzarenko@gmail.com>
2025-10-08 15:50:33 +0300

    feat: part of code was moved (search bar elements)

--- src/components/elements/SearchBar.tsx
+++ src/components/elements/SearchBar.tsx
@@ -2,6 +2,53 @@ import { useState } from "react";
 import { PinV1, PinsDistance, People, Search } from "./vectors.tsx";
 import "../../styles/elements/SearchBar.scss";
 
+const SearchItems = () => {
+    return (
+        <>
+            <div className="search__item">
+                <div className="search__item__icon">
+                    <PinV1 />
+                </div>
+                <div className="search__item__wrap">
+                    <h3 className="search__title">Location</h3>
+                    <div className="search__description">
+                        Where are you going?
+                    </div>
+                </div>
+            </div>
+            <div className="search__item">
+                <div className="search__item__icon">
+                    <PinsDistance />
+                </div>
+                <div className="search__item__wrap">
+                    <h3 className="search__title">Distance</h3>
+                    <div className="search__description">Distance k/m</div>
+                </div>
+            </div>
+            <div className="search__item">
+                <div className="search__item__icon">
+                    <People />
+                </div>
+                <div className="search__item__wrap">
+                    <h3 className="search__title">Max People</h3>
+                    <div className="search__description">0</div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+const SearchInput = () => {
+    return (
+        <input
+            className="search__input"
+            type="text"
+            name="search"
+            placeholder="Search..."
+        />
+    );
+};
+
 const SearchBar = () => {
     const [isInput, setInput] = useState(false);
 
@@ -12,53 +59,6 @@ const SearchBar = () => {
             setInput(false);
         }
     }
-
-    const SearchItems = () => {
-        return (
-            <>
-                <div className="search__item">
-                    <div className="search__item__icon">
-                        <PinV1 />
-                    </div>
-                    <div className="search__item__wrap">
-                        <h3 className="search__title">Location</h3>
-                        <div className="search__description">
-                            Where are you going?
-                        </div>
-                    </div>
-                </div>
-                <div className="search__item">
-                    <div className="search__item__icon">
-                        <PinsDistance />
-                    </div>
-                    <div className="search__item__wrap">
-                        <h3 className="search__title">Distance</h3>
-                        <div className="search__description">Distance k/m</div>
-                    </div>
-                </div>
-                <div className="search__item">
-                    <div className="search__item__icon">
-                        <People />
-                    </div>
-                    <div className="search__item__wrap">
-                        <h3 className="search__title">Max People</h3>
-                        <div className="search__description">0</div>
-                    </div>
-                </div>
-            </>
-        );
-    };
-
-    const SearchInput = () => {
-        return (
-            <input
-                className="search__input"
-                type="text"
-                name="search"
-                placeholder="Search..."
-            />
-        );
-    };
 
     return (
         <div className="search__bar">
