4bc355d1fc702363c4887b4d958cbcf7e2c38811
bogdan-nazarenko <bogdannazzarenko@gmail.com>
2025-10-10 22:01:06 +0300

    feat: services items with props (improvement)

--- src/components/blocks/Services.tsx
+++ src/components/blocks/Services.tsx
@@ -1,5 +1,17 @@
 import "../../styles/blocks/Services.scss";
 
+const ServicesItem = ({ title }: { title: string }) => {
+    return (
+        <div className="services__item">
+            <div className="services__item__image"></div>
+            <h3 className="services__item__title">{title}</h3>
+            <p className="block__text text--v3">
+                Lorem ipsum dolor sit amet, adipisicing elit.
+            </p>
+        </div>
+    );
+};
+
 const Services = ({ suptitleColor }: { suptitleColor: string }) => {
     return (
         <section className="services_section">
@@ -16,31 +28,9 @@ const Services = ({ suptitleColor }: { suptitleColor: string }) => {
                     </h2>
                 </div>
                 <div className="services__elements">
-                    <div className="services__item">
-                        <div className="services__item__image"></div>
-                        <h3 className="services__item__title">
-                            Calculate Weather
-                        </h3>
-                        <p className="block__text text--v3">
-                            Lorem ipsum dolor sit amet, adipisicing elit.
-                        </p>
-                    </div>
-                    <div className="services__item">
-                        <div className="services__item__image"></div>
-                        <h3 className="services__item__title">
-                            Best Tour Guide
-                        </h3>
-                        <p className="block__text text--v3">
-                            Lorem ipsum dolor sit amet, adipisicing elit.
-                        </p>
-                    </div>
-                    <div className="services__item">
-                        <div className="services__item__image"></div>
-                        <h3 className="services__item__title">Customization</h3>
-                        <p className="block__text text--v3">
-                            Lorem ipsum dolor sit amet, adipisicing elit.
-                        </p>
-                    </div>
+                    <ServicesItem title="Calculate Weather" />
+                    <ServicesItem title="Best Tour Guide" />
+                    <ServicesItem title="Customization" />
                 </div>
             </div>
         </section>
