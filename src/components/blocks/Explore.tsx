68c74c13af5c0f1d6ec42343d51ff499ff69f4cc
bogdan-nazarenko <bogdannazzarenko@gmail.com>
2025-10-10 22:21:01 +0300

    feat: explore images with props (improvement)

--- src/components/blocks/Explore.tsx
+++ src/components/blocks/Explore.tsx
@@ -7,6 +7,32 @@ import paris from "../../assets/media/images/paris.jpg";
 import wuxi from "../../assets/media/images/wuxi.jpg";
 import "../../styles/blocks/Explore.scss";
 
+interface ExploreImageProps {
+    suptitle: string;
+    title: string;
+    image: string;
+    alt: string;
+}
+
+const ExploreImage = ({ suptitle, title, image, alt }: ExploreImageProps) => {
+    return (
+        <div className="explore__image">
+            <div className="explore__captions__wrap">
+                <div className="explore__image__captions">
+                    <h3 className="explore__image__suptitle">{suptitle}</h3>
+                    <h3 className="explore__image__title">{title}</h3>
+                </div>
+            </div>
+            <img
+                loading="lazy"
+                className="explore__pic"
+                src={image}
+                alt={alt}
+            />
+        </div>
+    );
+};
+
 const Explore = () => {
     return (
         <section className="explore_section">
@@ -21,137 +47,53 @@ const Explore = () => {
                 </div>
                 <div className="explore__images">
                     <div className="explore__column">
-                        <div className="explore__image">
-                            <div className="explore__captions__wrap">
-                                <div className="explore__image__captions">
-                                    <h3 className="explore__image__suptitle">
-                                        Big Ben
-                                    </h3>
-                                    <h3 className="explore__image__title">
-                                        London
-                                    </h3>
-                                </div>
-                            </div>
-                            <img
-                                loading="lazy"
-                                className="explore__pic"
-                                src={london}
-                                alt="London"
-                            />
-                        </div>
-                        <div className="explore__image">
-                            <div className="explore__captions__wrap">
-                                <div className="explore__image__captions">
-                                    <h3 className="explore__image__suptitle">
-                                        Dubai
-                                    </h3>
-                                    <h3 className="explore__image__title">
-                                        UAE
-                                    </h3>
-                                </div>
-                            </div>
-                            <img
-                                loading="lazy"
-                                className="explore__pic"
-                                src={dubai}
-                                alt="Dubai"
-                            />
-                        </div>
+                        <ExploreImage
+                            suptitle="Big Ben"
+                            title="London"
+                            image={london}
+                            alt="London"
+                        />
+                        <ExploreImage
+                            suptitle="Dubai"
+                            title="UAE"
+                            image={dubai}
+                            alt="Dubai"
+                        />
                     </div>
                     <div className="explore__column">
-                        <div className="explore__image">
-                            <div className="explore__captions__wrap">
-                                <div className="explore__image__captions">
-                                    <h3 className="explore__image__suptitle">
-                                        Bali
-                                    </h3>
-                                    <h3 className="explore__image__title">
-                                        Indonesia
-                                    </h3>
-                                </div>
-                            </div>
-                            <img
-                                loading="lazy"
-                                className="explore__pic"
-                                src={bali}
-                                alt="Bali"
-                            />
-                        </div>
-                        <div className="explore__image">
-                            <div className="explore__captions__wrap">
-                                <div className="explore__image__captions">
-                                    <h3 className="explore__image__suptitle">
-                                        Phetchabun
-                                    </h3>
-                                    <h3 className="explore__image__title">
-                                        Thailand
-                                    </h3>
-                                </div>
-                            </div>
-                            <img
-                                loading="lazy"
-                                className="explore__pic"
-                                src={phetchabun}
-                                alt="Phetchabun"
-                            />
-                        </div>
+                        <ExploreImage
+                            suptitle="Bali"
+                            title="Indonesia"
+                            image={bali}
+                            alt="Bali"
+                        />
+                        <ExploreImage
+                            suptitle="Phetchabun"
+                            title="Thailand"
+                            image={phetchabun}
+                            alt="Phetchabun"
+                        />
                     </div>
                     <div className="explore__column">
-                        <div className="explore__image">
-                            <div className="explore__captions__wrap">
-                                <div className="explore__image__captions">
-                                    <h3 className="explore__image__suptitle">
-                                        Sydney
-                                    </h3>
-                                    <h3 className="explore__image__title">
-                                        Australia
-                                    </h3>
-                                </div>
-                            </div>
-                            <img
-                                loading="lazy"
-                                className="explore__pic"
-                                src={sydney}
-                                alt="Sydney"
-                            />
-                        </div>
+                        <ExploreImage
+                            suptitle="Sydney"
+                            title="Australia"
+                            image={sydney}
+                            alt="Sydney"
+                        />
                         <div className="explore__column__group">
-                            <div className="explore__image">
-                                <div className="explore__captions__wrap">
-                                    <div className="explore__image__captions">
-                                        <h3 className="explore__image__suptitle">
-                                            Paris
-                                        </h3>
-                                        <h3 className="explore__image__title">
-                                            France
-                                        </h3>
-                                    </div>
-                                </div>
-                                <img
-                                    loading="lazy"
-                                    className="explore__pic"
-                                    src={paris}
-                                    alt="Paris"
-                                />
-                            </div>
-                            <div className="explore__image">
-                                <div className="explore__captions__wrap">
-                                    <div className="explore__image__captions">
-                                        <h3 className="explore__image__suptitle">
-                                            Wuxi
-                                        </h3>
-                                        <h3 className="explore__image__title">
-                                            China
-                                        </h3>
-                                    </div>
-                                </div>
-                                <img
-                                    loading="lazy"
-                                    className="explore__pic"
-                                    src={wuxi}
-                                    alt="Wuxi"
-                                />
-                            </div>
+                            <ExploreImage
+                                suptitle="Paris"
+                                title="France"
+                                image={paris}
+                                alt="Paris"
+                            />
+                            <ExploreImage
+                                suptitle="Wuxi"
+                                title="China"
+                                image={wuxi}
+                                alt="Wuxi"
+                            />
                         </div>
                     </div>
                 </div>
