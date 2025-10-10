c96408ea0d9b58ca030a1e2a5e5f4212dbf6e9d4
bogdan-nazarenko <bogdannazzarenko@gmail.com>
2025-10-10 20:07:11 +0300

    feat: class removed from Plane (svg)

--- src/components/elements/vectors.tsx
+++ src/components/elements/vectors.tsx
@@ -160,7 +160,6 @@ export const Instagram = () => {
 export const Plane = () => {
     return (
         <svg
-            className="plane"
             width="448"
             height="133"
             viewBox="0 0 448 133"
