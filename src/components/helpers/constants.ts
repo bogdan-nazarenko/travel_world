30de53391a9d81cf871948b99a4f9fa91c632ad3
bogdan-nazarenko <bogdannazzarenko@gmail.com>
2025-11-02 23:01:33 +0200

    feat: added an object and interface with URL paths of app pages

--- src/components/helpers/constants.ts
+++ src/components/helpers/constants.ts
@@ -1,3 +1,14 @@
+import type { PagePaths } from "./interfaces.ts";
+
 // modifiers
 export const active: string = "active";
 export const open: string = "open";
+
+// objects
+export const pages: PagePaths = {
+    home: "/",
+    about: "/about",
+    tours: "/tours",
+    login: "/login",
+    register: "/register",
+};
---
+++ src/components/helpers/interfaces.ts
@@ -0,0 +1,7 @@
+export interface PagePaths {
+    home: string;
+    about: string;
+    tours: string;
+    login: string;
+    register: string;
+}
