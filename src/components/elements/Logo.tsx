5c5bc5b581025a0e97b87e55ac8d6d53962cf382
bogdan-nazarenko <bogdannazzarenko@gmail.com>
2025-11-06 02:09:07 +0200

    feat: ClickProps interface used (Logo)

--- src/components/elements/Logo.tsx
+++ src/components/elements/Logo.tsx
@@ -1,8 +1,9 @@
 import { Link } from "react-router-dom";
+import type { ClickProps } from "../helpers/interfaces.ts";
 import logo from "../../assets/media/images/logo.png";
 import "../../styles/elements/Logo.scss";
 
-const Logo = ({ clickFunc }: { clickFunc?: () => void }) => {
+const Logo = ({ clickFunc }: ClickProps) => {
     return (
         <Link className="logo" to="/" onClick={clickFunc}>
             <img className="logo__pic" src={logo} alt="Travel World" />
