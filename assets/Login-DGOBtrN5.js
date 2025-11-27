98f7691f9920222b44233cf8f4a1be066255edf6
bogdan-nazarenko <bogdannazzarenko@gmail.com>
2025-11-27 23:30:20 +0200

    Updates

---
+++ assets/Login-DGOBtrN5.js
@@ -0,0 +1,1 @@
+import{j as o}from"./vendor-DR72dedl.js";import{A as t}from"./Authorization-CAvjEV7F.js";import"./index-BZFR1e5s.js";const m=()=>o.jsx(t,{});export{m as default};
---
+++ assets/Register-DkZJvyE-.js
@@ -0,0 +1,1 @@
+import{j as t}from"./vendor-DR72dedl.js";import{A as r}from"./Authorization-CAvjEV7F.js";import"./index-BZFR1e5s.js";const e=()=>t.jsx(r,{});export{e as default};
