e11fb24fc0d11194138430b9e7e1fe6447929e90
bogdan-nazarenko <bogdannazzarenko@gmail.com>
2025-12-03 14:31:14 +0200

    Updates

---
+++ assets/Login-CejjG9gq.js
@@ -0,0 +1,1 @@
+import{j as o}from"./vendor-Cj163IS-.js";import{A as t}from"./Authorization-BlrxqWon.js";import"./index-C-_PMVZz.js";const m=()=>o.jsx(t,{});export{m as default};
---
+++ assets/Register-aF8PfxOA.js
@@ -0,0 +1,1 @@
+import{j as t}from"./vendor-Cj163IS-.js";import{A as r}from"./Authorization-BlrxqWon.js";import"./index-C-_PMVZz.js";const e=()=>t.jsx(r,{});export{e as default};
