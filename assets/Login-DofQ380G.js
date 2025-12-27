b8e2a650dd3de8e030e1a5a33a40a71de5d4061a
bogdan-nazarenko <bogdannazzarenko@gmail.com>
2025-12-27 05:05:35 +0200

    Updates

---
+++ assets/Login-DofQ380G.js
@@ -0,0 +1,1 @@
+import{j as o}from"./vendor-Cibptihg.js";import{A as t}from"./Authorization-Dv_L0udG.js";import"./index-Bc1eHCn-.js";const m=()=>o.jsx(t,{});export{m as default};
---
+++ assets/Register-QvUiLSnM.js
@@ -0,0 +1,1 @@
+import{j as t}from"./vendor-Cibptihg.js";import{A as r}from"./Authorization-Dv_L0udG.js";import"./index-Bc1eHCn-.js";const e=()=>t.jsx(r,{});export{e as default};
