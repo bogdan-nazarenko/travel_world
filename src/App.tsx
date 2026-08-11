51b42d800cf556c1a084d6437745d2cc3e4f3c0a
bogdan-nazarenko <bogdannazzarenko@gmail.com>
2026-08-11 20:43:21 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import Section from "@markup/Section";
+
+const App = () => {
+    return <Section />;
+};
+
+export default App;
---
+++ src/markup/Section.tsx
@@ -0,0 +1,11 @@
+const Section = () => {
+    return (
+        <section>
+            <div className="container">
+                <h1>React + Vite + SCSS + TS</h1>
+            </div>
+        </section>
+    );
+};
+
+export default Section;
