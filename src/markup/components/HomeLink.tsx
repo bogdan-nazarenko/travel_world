d31268c7d1817824ce2065224333f4417236d0d7
bogdan-nazarenko <bogdannazzarenko@gmail.com>
2026-08-14 00:30:13 +0300

    feat(markup): add HomeLink and logo

---
+++ src/markup/components/HomeLink.tsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router";
+import logo from "@media/images/logo.png";
+import "./HomeLink.scss";
+
+interface HomeLink {
+    className?: string;
+    onClick?: () => void;
+}
+
+const HomeLink = ({ className, onClick }: HomeLink) => {
+    return (
+        <Link
+            className={`${className || ""} home-link`.trim()}
+            to="/"
+            onClick={onClick}
+        >
+            <img className="logo" src={logo} alt="TravelWorld" />
+        </Link>
+    );
+};
+
+export default HomeLink;
