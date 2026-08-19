dd669598498de2ed39d2941798e9ab20bf456d94
bogdan-nazarenko <bogdannazzarenko@gmail.com>
2026-08-19 17:04:31 +0300

    feat(app): add home page
    - Use lazy import for the home page
    - Use Suspense with a loading indicator
    - Use routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import { lazy, Suspense } from "react";
+import { Routes, Route } from "react-router";
 import Header from "@markup/layout/Header";
+import Loading from "@markup/components/Loading";
+const Home = lazy(() => import("@markup/pages/Home"));
 import Footer from "@markup/layout/Footer";
 
 const App = () => {
     return (
         <>
             <Header />
-            <main className="main"></main>
+            <main className="main">
+                <Suspense fallback={<Loading />}>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                    </Routes>
+                </Suspense>
+            </main>
             <Footer />
         </>
     );
