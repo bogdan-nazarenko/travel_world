92d3197241eb13ea3707a3475d02a93291d9e9a7
bogdan-nazarenko <bogdannazzarenko@gmail.com>
2026-08-28 20:43:31 +0300

    feat(markup/sections): add Services
    - Add three instances of the Feature component with props
    - Add the section to the home page

--- src/markup/pages/Home.tsx
+++ src/markup/pages/Home.tsx
@@ -1,9 +1,11 @@
 import Hero from "@markup/sections/Hero";
+import Services from "@markup/sections/Services";
 
 const Home = () => {
     return (
         <>
             <Hero />
+            <Services />
         </>
     );
 };
---
+++ src/markup/sections/Services.tsx
@@ -0,0 +1,57 @@
+import { useLocation } from "react-router";
+import Feature, { type FeatureProps } from "@markup/components/Feature";
+import "./Services.scss";
+
+const features: FeatureProps[] = [
+    {
+        iconModifier: "feature__icon_weather",
+        title: "Calculate Weather",
+        text: "Lorem ipsum dolor sit amet, adipisicing elit.",
+    },
+    {
+        iconModifier: "feature__icon_guide",
+        title: "Best Tour Guide",
+        text: "Lorem ipsum dolor sit amet, adipisicing elit.",
+    },
+    {
+        iconModifier: "feature__icon_custom",
+        title: "Customization",
+        text: "Lorem ipsum dolor sit amet, adipisicing elit.",
+    },
+];
+
+const Services = () => {
+    const { pathname } = useLocation();
+
+    return (
+        <section className="services section">
+            <div className="services__container container">
+                <div className="services__info">
+                    <span
+                        className={`services__superscription ${pathname !== "/" ? "services__superscription_v2" : ""}`.trim()}
+                    >
+                        What we serve
+                    </span>
+                    <h2 className="title">We offer our best services</h2>
+                </div>
+                <div className="services__feature-group">
+                    {features.map((props) => {
+                        const { iconModifier, title, text } = props;
+
+                        return (
+                            <Feature
+                                key={title}
+                                className="services__feature"
+                                iconModifier={iconModifier}
+                                title={title}
+                                text={text}
+                            />
+                        );
+                    })}
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Services;
