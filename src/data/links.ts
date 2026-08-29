baf024e80f544a2315052c02b24d3d6fbf491523
bogdan-nazarenko <bogdannazzarenko@gmail.com>
2026-08-29 19:50:06 +0300

    feat(data/links): add tourLinkData

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,3 +1,6 @@
+import { type TourLinkProps } from "@markup/components/TourLink";
+import { tourCatalog } from "@data/images";
+
 export interface NavLink {
     targetId?: string;
     url: string;
@@ -13,4 +16,67 @@ export const mainLinks: NavLink[] = [
 export const authLinks: NavLink[] = [
     { url: "/login", name: "Login" },
     { url: "/register", name: "Register" },
+] as const;
+
+export const tourLinkData: TourLinkProps[] = [
+    {
+        url: "#",
+        image: tourCatalog.london,
+        title: "Big Ben",
+        location: "London",
+    },
+    {
+        url: "#",
+        image: tourCatalog.dubai,
+        title: "Dubai",
+        location: "UAE",
+    },
+    {
+        url: "#",
+        image: tourCatalog.bali,
+        title: "Bali",
+        location: "Indonesia",
+    },
+    {
+        url: "#",
+        image: tourCatalog.phetchabun,
+        title: "Phetchabun",
+        location: "Thailand",
+    },
+    {
+        url: "#",
+        image: tourCatalog.sydney,
+        title: "Sydney",
+        location: "Australia",
+    },
+    {
+        url: "#",
+        image: tourCatalog.paris,
+        title: "Paris",
+        location: "France",
+    },
+    {
+        url: "#",
+        image: tourCatalog.wuxi,
+        title: "Wuxi",
+        location: "China",
+    },
+    {
+        url: "#",
+        image: tourCatalog.maafushi,
+        title: "Maafushi Island",
+        location: "Maldives",
+    },
+    {
+        url: "#",
+        image: tourCatalog.phiphi,
+        title: "Phi Phi Island",
+        location: "Maldives",
+    },
+    {
+        url: "#",
+        image: tourCatalog.elNido,
+        title: "El Nido",
+        location: "Philippines",
+    },
 ] as const;
