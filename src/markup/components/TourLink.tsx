ffcf7dd852ca63aa0e1b1af6dd352397cd961c61
bogdan-nazarenko <bogdannazzarenko@gmail.com>
2026-08-29 16:38:21 +0300

    feat(markup/components): add TourLink

---
+++ src/markup/components/TourLink.tsx
@@ -0,0 +1,36 @@
+import { Link } from "react-router";
+import "./TourLink.scss";
+
+export interface TourLinkProps {
+    className?: string;
+    url: string;
+    image: string;
+    title: string;
+    location: string;
+}
+
+const TourLink = ({
+    className = "",
+    url = "#",
+    image,
+    title,
+    location,
+}: TourLinkProps) => {
+    return (
+        <Link
+            className={`${className} tour-link`.trim()}
+            to={url}
+            onClick={
+                url === "#" ? (event) => event.preventDefault() : undefined
+            }
+        >
+            <img className="tour-link__image" src={image} alt="" />
+            <div className="tour-link__content">
+                <span className="tour-link__title">{title}</span>
+                <span className="tour-link__location">{location}</span>
+            </div>
+        </Link>
+    );
+};
+
+export default TourLink;
