1e98531c54b894e2f4fc1f02980b93c3fc31cd7d
bogdan-nazarenko <bogdannazzarenko@gmail.com>
2026-08-29 13:34:50 +0300

    feat(data/images): add tour catalog

---
+++ src/data/images.ts
@@ -0,0 +1,33 @@
+import sydney from "@media/images/sydney.jpg";
+import wuxi from "@media/images/wuxi.jpg";
+import elNido from "@media/images/el-nido.jpg";
+import bali from "@media/images/bali.jpg";
+import mangunan from "@media/images/mangunan.jpg";
+import phetchabun from "@media/images/phetchabun.jpg";
+import phiphi from "@media/images/phiphi.jpg";
+import manali from "@media/images/manali.jpg";
+import maldives from "@media/images/maldives.jpg";
+import maafushi from "@media/images/maafushi.jpg";
+import dubai from "@media/images/dubai.jpg";
+import pamukkale from "@media/images/pamukkale.jpg";
+import krakow from "@media/images/krakow.jpg";
+import paris from "@media/images/paris.jpg";
+import london from "@media/images/london.jpg";
+
+export const tourCatalog = {
+    sydney,
+    wuxi,
+    elNido,
+    bali,
+    mangunan,
+    phetchabun,
+    phiphi,
+    manali,
+    maldives,
+    maafushi,
+    dubai,
+    pamukkale,
+    krakow,
+    paris,
+    london,
+};
