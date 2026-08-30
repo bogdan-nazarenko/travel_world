8758e64b6025442f1cccd5501a52bf07f9493b3b
bogdan-nazarenko <bogdannazzarenko@gmail.com>
2026-08-30 23:46:00 +0300

    feat(markup/sections): add Explore
    - Add seven instances of the TourLink component with props
    - Add the section to the home page

--- src/markup/pages/Home.tsx
+++ src/markup/pages/Home.tsx
@@ -1,11 +1,13 @@
 import Hero from "@markup/sections/Hero";
 import Services from "@markup/sections/Services";
+import Explore from "@markup/sections/Explore";
 
 const Home = () => {
     return (
         <>
             <Hero />
             <Services />
+            <Explore />
         </>
     );
 };
---
+++ src/markup/sections/Explore.tsx
@@ -0,0 +1,83 @@
+import { tourLinkData } from "@data/links";
+import TourLink from "@markup/components/TourLink";
+import "./Explore.scss";
+
+const bigBen = tourLinkData.find((link) => link.title === "Big Ben");
+const dubai = tourLinkData.find((link) => link.title === "Dubai");
+const bali = tourLinkData.find((link) => link.title === "Bali");
+const phetchabun = tourLinkData.find((link) => link.title === "Phetchabun");
+const sydney = tourLinkData.find((link) => link.title === "Sydney");
+const paris = tourLinkData.find((link) => link.title === "Paris");
+const wuxi = tourLinkData.find((link) => link.title === "Wuxi");
+
+const Explore = () => {
+    return (
+        <section className="explore section">
+            <div className="container">
+                <div className="explore__info">
+                    <span className="superscription">Explore</span>
+                    <h2 className="title title_v2">Our featured tours</h2>
+                </div>
+                <div className="explore__catalog">
+                    <div className="explore__column">
+                        <TourLink
+                            className="explore__tour-link"
+                            url={bigBen!.url}
+                            image={bigBen!.image}
+                            title={bigBen!.title}
+                            location={bigBen!.location}
+                        />
+                        <TourLink
+                            className="explore__tour-link"
+                            url={dubai!.url}
+                            image={dubai!.image}
+                            title={dubai!.title}
+                            location={dubai!.location}
+                        />
+                    </div>
+                    <div className="explore__column">
+                        <TourLink
+                            className="explore__tour-link"
+                            url={bali!.url}
+                            image={bali!.image}
+                            title={bali!.title}
+                            location={bali!.location}
+                        />
+                        <TourLink
+                            className="explore__tour-link"
+                            url={phetchabun!.url}
+                            image={phetchabun!.image}
+                            title={phetchabun!.title}
+                            location={phetchabun!.location}
+                        />
+                    </div>
+                    <div className="explore__column">
+                        <TourLink
+                            className="explore__tour-link explore__tour-link_sydney"
+                            url={sydney!.url}
+                            image={sydney!.image}
+                            title={sydney!.title}
+                            location={sydney!.location}
+                        />
+                        <TourLink
+                            className="explore__tour-link"
+                            url={paris!.url}
+                            image={paris!.image}
+                            title={paris!.title}
+                            location={paris!.location}
+                        />
+                        <TourLink
+                            className="explore__tour-link"
+                            url={wuxi!.url}
+                            image={wuxi!.image}
+                            title={wuxi!.title}
+                            location={wuxi!.location}
+                        />
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Explore;
